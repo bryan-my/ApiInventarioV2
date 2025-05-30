2f2845b51ae81e03f86888d07af0ac6d293929fd
Bryan JImenez <bry.jimenez@duocuc.cl>
2025-05-30 18:30:02 -0400

    Creacion Api administracion Inventario

---
+++ src/main/java/com/ecomarket/ApiInventarioV2/ApiInventarioV2Application.java
@@ -0,0 +1,13 @@
+package com.ecomarket.ApiInventarioV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInventarioV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInventarioV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomarket/ApiInventarioV2/ApiInventarioV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.ApiInventarioV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventarioV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
